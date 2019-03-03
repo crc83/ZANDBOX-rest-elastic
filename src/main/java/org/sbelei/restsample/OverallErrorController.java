8963dc09073bea56392f07456bd16bbb95b28ef0
Serhii Belei <sergiy.beley@gmail.com>
2019-03-03 23:22:08 +0200

    error controller and logback configuration

--- src/main/java/org/sbelei/restsample/OverallErrorController.java
+++ src/main/java/org/sbelei/restsample/OverallErrorController.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 public class OverallErrorController implements ErrorController{
 
     Logger log = LoggerFactory.getLogger(OverallErrorController.class);
